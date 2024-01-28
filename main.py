2bf767ef82c1c1ec1212098497b318fa2134e572
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-01-28 00:03:46 -0500

    Switched to an async API setup

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-from tiktokapipy.api import TikTokAPI
+from tiktokapipy.async_api import AsyncTikTokAPI
 from tiktokapipy.models.video import Video
 from tiktokapipy.models.challenge import Challenge
 from pprint import pprint
+import asyncio
 
 # TikTokApiPy: https://tiktokpy.readthedocs.io/en/latest/reference/api_reference.html
 
 tag_name: str = "climatechange"
 limit: int = 10
 
-def get_tiktok_test():
+async def get_tiktok_test():
     tiktok_collection = []
 
-    with TikTokAPI() as api:
+    async with AsyncTikTokAPI() as api:
         # Challenge is the TikTok internal name for a hashtag
-        challenge: Challenge = api.challenge(tag_name, video_limit=10)
+        challenge: Challenge = await api.challenge(tag_name, video_limit=10)
 
-        for video in challenge.videos: 
+        async for video in challenge.videos: 
             assert isinstance(video, Video) 
             data = {
                 "desc": video.desc,
@@ -29,7 +30,7 @@ def get_tiktok_test():
 
 
 def main():
-    results = get_tiktok_test()
+    results = asyncio.run(get_tiktok_test())
     pprint(results)
 
 
