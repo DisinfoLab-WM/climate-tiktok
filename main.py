caf05aa42a728494eaa9e7bd7c4d4377e05f4230
Ian MacDonald <ianmacdonaldm@gmail.com>
2024-01-28 00:12:06 -0500

    Formatted and linted project

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from tiktokapipy.async_api import AsyncTikTokAPI
-from tiktokapipy.models.video import Video
-from tiktokapipy.models.challenge import Challenge
-from pprint import pprint
 import asyncio
+from pprint import pprint
+
+from tiktokapipy.async_api import AsyncTikTokAPI
+from tiktokapipy.models.challenge import Challenge
+from tiktokapipy.models.video import Video
 
 # TikTokApiPy: https://tiktokpy.readthedocs.io/en/latest/reference/api_reference.html
 
 tag_name: str = "climatechange"
 limit: int = 10
+
 
 async def get_tiktok_test():
     tiktok_collection = []
@@ -16,13 +18,13 @@ async def get_tiktok_test():
         # Challenge is the TikTok internal name for a hashtag
         challenge: Challenge = await api.challenge(tag_name, video_limit=10)
 
-        async for video in challenge.videos: 
-            assert isinstance(video, Video) 
+        async for video in challenge.videos:
+            assert isinstance(video, Video)
             data = {
                 "desc": video.desc,
                 "plays": video.stats.play_count,
                 "comments": video.stats.comment_count,
-                "url": video.url
+                "url": video.url,
             }
             tiktok_collection.append(data)
 
@@ -34,5 +36,5 @@ def main():
     pprint(results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
